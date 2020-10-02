5cffb78459f2093bbb1774b31872c7c1aa8cb3e4
nstammewar <nstammewar@gmail.com>
2020-10-02 18:51:26 +0530

    Mering the Git Pod Changes

--- src/main/java/com/reactInmage/aws_image_upload/AwsImageUploadApplication.java
+++ src/main/java/com/reactInmage/aws_image_upload/AwsImageUploadApplication.java
@@ -2,8 +2,14 @@ package com.reactInmage.aws_image_upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:auth.properties")
+})
 public class AwsImageUploadApplication {
 
 	public static void main(String[] args) {
